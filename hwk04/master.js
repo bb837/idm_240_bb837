92267564c68828bba41b234f12a5e20548895561
bb837 <43794585+bb837@users.noreply.github.com>
2019-05-28 13:36:08 -0400

    update to content
    audio toggle working, final touches

--- hwk04/master.js
+++ hwk04/master.js
@@ -1,4 +1,5 @@
 var audioToggle = document.getElementById('mic');
+var mute = document.getElementById("mic-off");
 var music = document.getElementById("music");
 var sound = false;
 
@@ -6,10 +7,12 @@ function playSound () {
     if (sound) {
         music.pause();
         sound = false;
+        mute.classList.remove('hidden');
         console.log(sound);
     } else {
         music.play(); 
         sound = true;
+        mute.classList.add('hidden');
         console.log(sound);
     }
 }
